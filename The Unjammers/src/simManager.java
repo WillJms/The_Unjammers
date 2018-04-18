7b57d465df841ab74ff9ed526d97f861353b11df
Will James <jamesw2@my.erau.edu>
2018-04-18 14:40:20 -0400

    construction on file io began

--- The Unjammers/src/simManager.java
+++ The Unjammers/src/simManager.java
@@ -1,8 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class simManager {
 
 	//TODO parameters for vehicle randomization
+	int numVehicles;
 	//TODO parameters for lane locations
 	//TODO parameters for sings
 	//TODO parameters for circle
+	String file = "config.txt";
+	
+	public void parseConfigFile(String file){
+		
+	    try {
+
+	        Scanner sc = new Scanner(file);
+	        while (sc.hasNextLine()) {
+	        	//String line = sc.nextLine();
+	        	
+	        	switch (sc.next()) {
+	        	
+	        		case "vehicles":
+	        			numVehicles = sc.nextInt();
+	        			break;
+	        		
+	        		
+	        			
+	        	}
+	        	
+	        }
+	        sc.close();
+	    } 
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	 }
+
+	
 }
