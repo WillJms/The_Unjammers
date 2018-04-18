b0ba5ae5d3d61dec58923797dec36d21b96d3cb2
Will James <jamesw2@my.erau.edu>
2018-04-18 16:08:04 -0400

    switch implemitation started

--- The Unjammers/src/simManager.java
+++ The Unjammers/src/simManager.java
@@ -6,6 +6,23 @@ public class simManager {
 
 	//TODO parameters for vehicle randomization
 	int numVehicles;
+	int minCarLength;
+	int maxLength;
+	int avLength;
+	int minXLength;
+	int avXLength;
+	int maxXLength;
+	int minSUVLength;
+	int avSUVLength;
+	int maxSUVLength;
+	int minTruckLength;
+	int avTruckLength;
+	int maxTruckLength;
+	int minITLength;
+	int avITLength;
+	int maxITLength;
+
+	
 	//TODO parameters for lane locations
 	//TODO parameters for sings
 	//TODO parameters for circle
@@ -21,10 +38,66 @@ public class simManager {
 	        	
 	        	switch (sc.next()) {
 	        	
-	        		case "vehicles":
+	        		case "numVehicles":
 	        			numVehicles = sc.nextInt();
 	        			break;
+	        			
+	        		case "minCarLength":
+	        			minCarLength = sc.nextInt();
+	        			break;
 	        		
+	        		case "maxCarLength" :
+	        			maxLength= sc.nextInt();
+	        			break;
+	        		
+	        		case "avCarLength":
+	        			avLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "minXLength":
+	        			minXLength = sc.nextInt();
+	        			break;
+	        		
+	        		case "avXLength":
+	        			avXLength = sc.nextInt();
+	        			break;
+	        		
+	        		case "maxXLength": 
+	        			maxXLength = sc.nextInt();
+	        			break;
+	        		
+	        		case "minSUVLength":
+	        			minSUVLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "avSUVLength":
+	        			avSUVLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "maxSUVLength":
+	        			maxSUVLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "minTruckLength":
+	        			minTruckLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "avTruckLength":
+	        			avTruckLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "maxTruckLength":
+	        			maxTruckLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "minITLength":
+	        			minITLength = sc.nextInt();
+	        			break;
+	        			
+	        		case "avITLength":
+	        			avITLength = sc.nextInt();
+	        			break;
+	        			
 	        		
 	        			
 	        	}
